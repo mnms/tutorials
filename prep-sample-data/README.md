78c667368637d9b4b7d24e6c5ed4d7bd1c144c0c
Junghwan Park <reserve.dev@gmail.com>
2019-11-12 22:07:53 +0900

    add script and sample files for generate sample data files

---
+++ prep-sample-data/README.md
@@ -0,0 +1,105 @@
+# Prepare sample data
+
+## Prerequisite
+
+First, Install requirements
+
+* Python3 and libraries for development
+  * `sudo yum install -y python3 python3-devel`
+* Python3 packages
+  * `pip3 install numpy pandas dask dask[dataframe] dask[distributed] toolz psutil`
+
+## Installation
+
+* Clone this repository
+  * `git clone https://github.com/mnms/tutorials`
+
+## Run script
+
+* Run Python script to generate
+  * `python3 prep.py`
+  * It may take 2~3 minutes for generate almost 51,800,000 rows with 24 files.
+
+## Options
+
+There're several options to generate sample data.
+
+| Argument     | Usage                | Default               | Description                                      |
+| ------------ | -------------------- | --------------------- | ------------------------------------------------ |
+| --help       | --help               |                       | show help message                                |
+| --start DTM  | --start 201912071230 | 201903011230          | set starting datetime with `yyyymmddHHMM` format |
+| --count INT  | --count 3            | 24                    | set how many files to generate                   |
+| --scale INT  | --scale 8            | 5                     | set how many rows to generate per file           |
+| --worker INT | --worker 12          | 8                     | set how many processes to run parallelly         |
+| --outdir DIR | --outdir /a/b/c/data | ./loadData            | set directory path to save generated files       |
+
+## Example
+
+* Command
+  * The following command creates `12 workers` that generate `30 files`.
+  * At this time, each file has a time value of 5 minutes intervals starting from `09: 00 on April 20, 1984`, and each file has more than `4M lines`.
+  * Also, the generated files are stored in `~/sample-data`.
+
+  ```sh
+  python3 prep.py --start 198404200900 --count 30 --scale 10 --worker 12 --outdir ~/sample-data
+  ```
+
+* Result
+  * The above command took 2 minutes and 35 seconds to run.
+  * Let's look at the output files.
+
+  ```sh
+    # Count how man rows
+    $ wc -l ~/sample-data/*.csv
+      4039084 /home/ec2-user/sample-data/198404200900.csv
+      4407299 /home/ec2-user/sample-data/198404200905.csv
+      4523700 /home/ec2-user/sample-data/198404200910.csv
+      4583207 /home/ec2-user/sample-data/198404200915.csv
+      4486553 /home/ec2-user/sample-data/198404200920.csv
+      4432518 /home/ec2-user/sample-data/198404200925.csv
+      4184087 /home/ec2-user/sample-data/198404200930.csv
+      4061729 /home/ec2-user/sample-data/198404200935.csv
+      4508797 /home/ec2-user/sample-data/198404200940.csv
+      4163869 /home/ec2-user/sample-data/198404200945.csv
+      4478236 /home/ec2-user/sample-data/198404200950.csv
+      4511293 /home/ec2-user/sample-data/198404200955.csv
+      4478304 /home/ec2-user/sample-data/198404201000.csv
+      4252531 /home/ec2-user/sample-data/198404201005.csv
+      4603777 /home/ec2-user/sample-data/198404201010.csv
+      4435624 /home/ec2-user/sample-data/198404201015.csv
+      4199445 /home/ec2-user/sample-data/198404201020.csv
+      4063136 /home/ec2-user/sample-data/198404201025.csv
+      4096386 /home/ec2-user/sample-data/198404201030.csv
+      4600275 /home/ec2-user/sample-data/198404201035.csv
+      4067459 /home/ec2-user/sample-data/198404201040.csv
+      4063233 /home/ec2-user/sample-data/198404201045.csv
+      4557936 /home/ec2-user/sample-data/198404201050.csv
+      4132154 /home/ec2-user/sample-data/198404201055.csv
+      4000512 /home/ec2-user/sample-data/198404201100.csv
+      4375469 /home/ec2-user/sample-data/198404201105.csv
+      4391156 /home/ec2-user/sample-data/198404201110.csv
+      4414736 /home/ec2-user/sample-data/198404201115.csv
+      4489189 /home/ec2-user/sample-data/198404201120.csv
+      4351782 /home/ec2-user/sample-data/198404201125.csv
+    129953476 total
+
+    # Take a look file
+    $ head ~/sample-data/198404201015.csv
+    event_time,name,sur_name,sex,city,lattitude,longitude,country
+    198404201015,Oneta,STAUNER,girl,Karachi,24.8700,66.9900,Pakistan
+    198404201015,Carry,GUZIK,girl,Bikin,46.8203,134.2649,Russia
+    198404201015,Alana,ANDRYSIAK,girl,Buizhou,37.3704,118.0200,China
+    198404201015,Vaughn,SMEAD,boy,Troy,42.7354,-73.6751,United States
+    198404201015,Billy,BIGHORN,boy,Mount Pleasant,32.8533,-79.8269,United States
+    198404201015,Stephan,LAUTHER,boy,La Puente,34.0323,-117.9533,United States
+    198404201015,Darrin,DEBARD,boy,Savoy,40.0602,-88.2553,United States
+    198404201015,Derick,HERMS,boy,Shaoxing,30.0004,120.5700,China
+    198404201015,Ramsey,BUSKER,boy,Kendu Bay,-0.3596,34.6400,Kenya
+  ```
+
+## License
+
+* Each file inside `./sampleData` directory retains its copyright.
+  * `baby-names.csv`: N/A from [hadley/data-baby-names](https://github.com/hadley/data-baby-names/blob/master/baby-names.csv)
+  * `surnames.csv`: [CC-BY-4.0](https://github.com/fivethirtyeight/data/blob/master/LICENSE) from [fivethirtyeight/data](https://github.com/fivethirtyeight/data/blob/master/most-common-name/surnames.csv)
+  * `worldcities.csv`: [CC-BY-4.0](https://creativecommons.org/licenses/by/4.0/) from [simplemaps.com](https://simplemaps.com/data/world-cities)
---
+++ prep-sample-data/prep.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import os
+import random
+import warnings
+import argparse
+
+import pandas as pd
+import dask.dataframe as dd
+from dask.distributed import Client
+
+
+# Sampling and generating single csv file (per event_time)
+def sample_and_write_csv(event_time, df_name, df_sname, df_geo, out_dir, scale_fcator=10):
+    ntarget = int(random.randint(4000000, 4614159) * scale_fcator / 10)
+    out_csv = os.path.join(out_dir, event_time + '.csv')
+
+    # Calculate frac ratio
+    frac_geo = ntarget / df_geo.shape[0].compute()
+    frac_name = ntarget / df_name.shape[0].compute()
+    frac_sname = ntarget / df_sname.shape[0].compute()
+
+    # Sampling from Dataframe
+    sample_geo = df_geo.sample(frac=frac_geo, replace=True).reset_index(drop=True)
+    sample_name = df_name.sample(frac=frac_name, replace=True).reset_index(drop=True)
+    sample_sname = df_sname.sample(frac=frac_sname, replace=True).reset_index(drop=True)
+
+    # Concat, assign event_dtm & reorder columns
+    result = dd.concat([sample_name, sample_sname, sample_geo], axis='columns')
+    result = result.assign(event_time=event_time).set_index('event_time')
+    result = result[['name', 'sur_name', 'sex', 'city', 'lattitude', 'longitude', 'country']]
+
+    # Write into one file
+    result_fn = result.to_csv(out_csv, compute=True, single_file=True)
+
+    # Free result values & return
+    del result
+    return result_fn
+
+
+# Generate csv files
+def run_helper(partition, df_name, df_sname, df_geo, out_dir, scale_factor=10):
+    results = []
+
+    for _, item in partition.iterrows():
+        event_time = item[0]
+        result = sample_and_write_csv(event_time, df_name, df_sname, df_geo, out_dir, scale_factor)
+        results.append(result)
+
+    return results
+
+
+# Parse arguments
+def arg_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--start', help='starting datetime in \'YYYYMMDDHHMMDD\' format \n(default: 201903011230)',
+                        default='201903011230', type=str)
+    parser.add_argument('--count', help='how many files to generate (default: 24)', default=24, type=int)
+    parser.add_argument('--scale', help='how large files to generate (default: 5)', default=5, type=int)
+    parser.add_argument('--worker', help='how many processes to process (default: 8)', default=8, type=int)
+    parser.add_argument('--outdir', help='dir path to save generated files (default: ./loadData)',
+                        default='./loadData', type=str)
+
+    args = parser.parse_args()
+
+    return args
+
+
+# main
+def main(client, arg):
+    # Parameters
+    datetime_start = args.start
+    file_count = args.count
+    scale_factor = args.scale
+    out_dir = args.outdir
+    npartitions = 24
+
+    # Make dir if not exists
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # Sample files' path & name
+    fn_geo = './sampleData/worldcities.csv'
+    fn_name = './sampleData/baby-names.csv'
+    fn_sname = './sampleData/surnames.csv'
+
+    # Samplel files' data types
+    meta_geo = {
+        'city': 'object',
+        'city_ascii': 'object',
+        'lat': 'object',
+        'lng': 'object',
+        'city': 'object',
+        'country': 'object',
+        'iso2': 'object',
+        'iso3': 'object',
+        'capital': 'object',
+        'population': 'float64',
+        'id': 'int64'
+    }
+
+    meta_name = {
+        'year': 'int64',
+        'name': 'object',
+        'percent': 'float64',
+        'sex': 'object'
+    }
+
+    meta_sname = {
+        'name': 'object',
+        'rank': 'int64',
+        'count': 'int64',
+        'prop100k': 'float64',
+        'cum_prop100k': 'float64',
+        'pctwhite': 'object',
+        'pctblack': 'object',
+        'pctapi': 'object',
+        'pctaian': 'object',
+        'pct2prace': 'object',
+        'pcthispanic': 'object'
+    }
+
+    meta_dtm = {
+        'event_time': 'int64'
+    }
+
+    # Read file as Pandas DF & Select columns
+    pdf_geo = pd.read_csv(fn_geo, dtype=meta_geo)[['city_ascii', 'lat', 'lng', 'country']]
+    pdf_name = pd.read_csv(fn_name, dtype=meta_name)[['name', 'sex']]
+    pdf_sname = pd.read_csv(fn_sname, dtype=meta_sname)[['name']]
+
+    # Rename columns
+    pdf_geo = pdf_geo.rename(columns={'city_ascii': 'city', 'lat': 'lattitude', 'lng': 'longitude'})
+    pdf_sname = pdf_sname.rename(columns={'name': 'sur_name'})
+
+    # Convert Pandas DFs to Dask DFs
+    df_geo = dd.from_pandas(pdf_geo, npartitions=1)
+    df_name = dd.from_pandas(pdf_name, npartitions=1)
+    df_sname = dd.from_pandas(pdf_sname, npartitions=1)
+
+    # Distribute sample DFs
+    # df_geo = await client.gather(client.scatter(df_geo, broadcast=True))
+    # df_name = await client.gather(client.scatter(df_name, broadcast=True))
+    # df_sname = await client.gather(client.scatter(df_sname, broadcast=True))
+    df_geo = client.scatter(df_geo, broadcast=True)
+    df_name = client.scatter(df_name, broadcast=True)
+    df_sname = client.scatter(df_sname, broadcast=True)
+
+    # Generate Datetime index & partition
+    datetime_format = '%Y%m%d%H%M'
+    list_dtm = pd.date_range(start=datetime_start, freq='5min', periods=file_count).strftime(datetime_format).to_list()
+    pdf_dtm = pd.DataFrame({'event_time': list_dtm}, columns=['event_time'])
+    df_dtm = dd.from_pandas(pdf_dtm, npartitions=npartitions).persist()
+
+    # Sample & generate csv file per datetime
+    out = df_dtm.map_partitions(run_helper, df_name, df_sname, df_geo, out_dir, scale_factor, meta=meta_dtm).map_partitions(len).compute()
+    nfiles = out.sum()
+
+    print("* {} files generated.".format(nfiles))
+
+
+if __name__ == '__main__':
+    # Parse arguments
+    args = arg_parser()
+
+    # Dask Client
+    with Client(n_workers=args.worker, threads_per_worker=2, processes=True) as client, warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=UserWarning)
+        main(client, args)
